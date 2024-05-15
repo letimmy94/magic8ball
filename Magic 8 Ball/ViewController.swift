327b254b5be0809cf397dd4222367ad7dcf472fd
Timmy Le <le.timmy94@gmail.com>
2024-05-15 17:47:33 -0400

    fixes

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -14,8 +14,8 @@ class ViewController: UIViewController {
     
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
 
-
-    @IBAction func askButtonPressed(_ sender: Any) {
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        ballArray.image = 
     }
     
 }
