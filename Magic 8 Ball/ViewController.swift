16b7360b7cd71c46720bd8b741e5aed7db711d24
Timmy Le <le.timmy94@gmail.com>
2024-05-17 13:57:03 -0400

    this might be broken

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -11,11 +11,11 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
-    ballArray.image = #imageLiteral(resourceName: "ball5.png")
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball2@2x.png"),#imageLiteral(resourceName: "ball2@3x.png")]
 
     @IBAction func askButtonPressed(_ sender: UIButton) {
+        ballArray.image = #imageLiteral(resourceName: "ball1")
     }
     
 }
